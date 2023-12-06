7002e4dbc24ea0a7ec0847ba8a8cbc6fe6bc597d
Alexander Kouyoumjian <123589862+AKouyoumjian@users.noreply.github.com>
2023-12-06 13:24:52 -0500

    added error handling for signin and signup

--- src/login/Signin.js
+++ src/login/Signin.js
@@ -7,42 +7,80 @@ import Nav from "../Nav";
 
 function Signin() {
   const [credentials, setCredentials] = useState({ username: "", password: "" });
+  const [showError, setShowError] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const signin = async () => {
+    if (!credentials.username || !credentials.password) {
+      setShowError(true);
+      setTimeout(() => {
+        setShowError(false);
+      }, 5000);
+      return;
+    }
     try {
       const user = await client.signin(credentials);
-      dispatch(setCurrentUser(user))
+      dispatch(setCurrentUser(user));
       navigate("/");
     } catch (error) {
       console.error("Sign-in failed:", error);
+      setShowError(true);
+      setTimeout(() => {
+        setShowError(false);
+      }, 5000);
+      return;
     }
   };
-  
+
   return (
     <div className="background-sign">
       <Nav />
+      {showError && ( // Displays error message if showError is true
+        <div className="alert alert-danger mt-3" role="alert">
+          "Login unsuccessful. Please try again."
+        </div>
+      )}
       <div className="container sign1">
         <div className="container sign">
           <div className="d-flex justify-content-center">
-          <h2 className="webtitle">Movie Website</h2>
+            <h2 className="webtitle">Movie Website</h2>
           </div>
-          <form>      
+          <form>
             <div className="form-group sign">
-              <input type="text" class="form-control" placeholder="Username" value={credentials.username} onChange={(e) => setCredentials({...credentials, username: e.target.value})} />
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Username"
+                value={credentials.username}
+                onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
+              />
             </div>
             <div className="form-group sign">
-              <input type="password" class="form-control" placeholder="Password" value={credentials.password} onChange={(e) => setCredentials({...credentials, password: e.target.value})}/>
+              <input
+                type="password"
+                class="form-control"
+                placeholder="Password"
+                value={credentials.password}
+                onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+              />
             </div>
-            <p className="sign-sent"> Don't have an account? <a href="#/register/*" className="sign-link">Sign up</a></p>
+            <p className="sign-sent">
+              {" "}
+              Don't have an account?{" "}
+              <a href="#/register/*" className="sign-link">
+                Sign up
+              </a>
+            </p>
             <div className="d-flex justify-content-center">
-              <button type="submit" class="btn-sign" onClick={signin}>Login</button>
+              <button type="submit" class="btn-sign" onClick={signin}>
+                Login
+              </button>
             </div>
           </form>
-        </div>  
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Signin;
--- src/login/Signup.js
+++ src/login/Signup.js
@@ -1,23 +1,71 @@
-import Nav from "../Nav"
+import Nav from "../Nav";
 import React, { useState } from "react";
 import * as client from "./client";
 import { useNavigate } from "react-router-dom";
 
-function Signup(){
+function Signup() {
   const navigate = useNavigate();
   const [credentials, setCredentials] = useState({
-     firstname:"", username: "", password: "",
-     email:"", role:"REVIEWER"});
+    name: "",
+    username: "",
+    password: "",
+    email: "",
+    role: "REVIEWER",
+  });
+  const [error, setError] = useState("");
+  const [showError, setShowError] = useState(false);
 
-    const handleRoleChange = (e) => {
+  const handleRoleChange = (e) => {
     setCredentials({ ...credentials, role: e.target.value });
   };
   const signup = async () => {
+    // if no username is inputted
+    if (!credentials.username) {
+      setError("Please enter a username.");
+      setShowError(true);
+      setTimeout(() => {
+        setShowError(false);
+      }, 5000);
+      return;
+    }
+    // if no name is inputted
+    if (!credentials.name) {
+      setError("Please enter a name.");
+      setShowError(true);
+      setTimeout(() => {
+        setShowError(false);
+      }, 5000);
+      return;
+    }
+    // if no email is inputted
+    if (!credentials.email) {
+      setError("Please enter a email.");
+      setShowError(true);
+      setTimeout(() => {
+        setShowError(false);
+      }, 5000);
+      return;
+    }
+    // if no password is inputted
+    if (!credentials.password) {
+      setError("Please enter a password.");
+      setShowError(true);
+      setTimeout(() => {
+        setShowError(false);
+      }, 5000);
+      return;
+    }
+
     try {
       await client.signup(credentials);
       navigate("/login");
     } catch (err) {
       console.log("error");
+      setError(err.response.data.message);
+      setShowError(true);
+      setTimeout(() => {
+        setShowError(false);
+      }, 5000);
     }
   };
 
@@ -27,52 +75,101 @@ function Signup(){
       <div className="container sign1">
         <div className="container sign">
           <div className="d-flex justify-content-center">
-          <h2 className="webtitle">Create an account</h2>
+            <h2 className="webtitle">Create an account</h2>
           </div>
-          <form>         
+          {showError && ( // Displays error message if showError is true
+            <div className="alert alert-danger mt-3" role="alert">
+              {error}
+            </div>
+          )}
+          <form>
             <div className="form-group sign">
-              <input type="text" class="form-control" placeholder="Username" value={credentials.username} 
-              onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}/>
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Username"
+                value={credentials.username}
+                onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
+              />
             </div>
             <div className="form-group sign">
-              <input type="email" class="form-control" placeholder="Email" value={credentials.email} 
-              onChange={(e) => setCredentials({ ...credentials, email: e.target.value })}/>
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Name"
+                value={credentials.name}
+                onChange={(e) => setCredentials({ ...credentials, name: e.target.value })}
+              />
             </div>
             <div className="form-group sign">
-              <input type="password" class="form-control" placeholder="Password" value={credentials.password} 
-              onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}/>
+              <input
+                type="email"
+                class="form-control"
+                placeholder="Email"
+                value={credentials.email}
+                onChange={(e) => setCredentials({ ...credentials, email: e.target.value })}
+              />
             </div>
             <div className="form-group sign">
-              <input type="password" class="form-control" placeholder="Confirm Password"/>
+              <input
+                type="password"
+                class="form-control"
+                placeholder="Password"
+                value={credentials.password}
+                onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+              />
+            </div>
+            <div className="form-group sign">
+              <input type="password" class="form-control" placeholder="Confirm Password" />
             </div>
             <div className="mb-3">
               <div className="form-check sign">
-                <input className="form-check-input" type="radio"
-                 name="TypeOfUser" id="Reviewer" value="REVIEWER"
-                checked={credentials.role === "REVIEWER"} onChange={handleRoleChange}/>
-                  <label className="register-sent" for="Reviewer">
-                    Reviewer
-                  </label>
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="TypeOfUser"
+                  id="Reviewer"
+                  value="REVIEWER"
+                  checked={credentials.role === "REVIEWER"}
+                  onChange={handleRoleChange}
+                />
+                <label className="register-sent" for="Reviewer">
+                  Reviewer
+                </label>
               </div>
               <div className="form-check sign">
-                <input className="form-check-input" type="radio"
-                 name="TypeOfUser" id="Director" value="DIRECTOR"
-                 checked={credentials.role === "DIRECTOR"} onChange={(handleRoleChange)}/>
-                  <label className="register-sent" for="Director">
-                    Director
-                  </label>
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="TypeOfUser"
+                  id="Director"
+                  value="DIRECTOR"
+                  checked={credentials.role === "DIRECTOR"}
+                  onChange={handleRoleChange}
+                />
+                <label className="register-sent" for="Director">
+                  Director
+                </label>
               </div>
             </div>
 
-            <p className="sign-sent"> Already have an account? <a href="#/login/*" className="sign-link">Login now</a></p>
+            <p className="sign-sent">
+              {" "}
+              Already have an account?{" "}
+              <a href="#/login/*" className="sign-link">
+                Login now
+              </a>
+            </p>
             <div className="d-flex justify-content-center">
-              <button type="submit" class="btn-sign" onClick={signup}>Sign Up</button>
+              <button type="submit" class="btn-sign" onClick={signup}>
+                Sign Up
+              </button>
             </div>
           </form>
-        </div>  
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Signup;
